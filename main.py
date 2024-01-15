ef78d710a9a1a9b0b05d78b265cdd07b161104a0
Daniel Houghton <danielhoughton1986@gmail.com>
2024-01-15 22:58:11 +0000

    added confirmation boxes and verification

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 
+from tkinter import messagebox
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -9,10 +11,23 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
-    with open("data.txt", "a") as data_file:
-        data_file.write(f"{website} | {email} | {password}\n")
-    website_entry.delete(0, tk.END)
-    password_entry.delete(0, tk.END)
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(
+            title="Oops", message="Please make sure you havent left any fields empty!"
+        )
+    else:
+        is_ok = messagebox.askokcancel(
+            title=website,
+            message=f"These are the details entered: \nEmail: {email}"
+            f"\nPassword: {password} \n Is it ok to save?",
+        )
+
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, tk.END)
+                password_entry.delete(0, tk.END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
